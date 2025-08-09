9b00573db4545696283a138ec6f3bcfc7a023cdd
Safiul Haque <mdsafiulhaque4@gmail.com>
2025-08-10 01:21:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
     <>
       <Navbar />
       <GlobalStyle>
-        {/* <LandingPageMain/> */}
-        <ProjectsMain/>
+        <LandingPageMain/>
+        {/* <ProjectsMain/> */}
       </GlobalStyle>
       <CopyrightSection/>
     </>
