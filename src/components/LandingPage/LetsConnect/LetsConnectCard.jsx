27b63837e27796787fe70a98bb0fe05e6b8ac7a9
Safiul Haque <mdsafiulhaque4@gmail.com>
2025-08-19 18:26:14 +0530

    links done

--- src/components/LandingPage/LetsConnect/LetsConnectCard.jsx
+++ src/components/LandingPage/LetsConnect/LetsConnectCard.jsx
@@ -7,18 +7,27 @@ import {
 } from "./Styles/LetsConnectCard.styled";
 import { colour_green, colour_orange, colour_white } from "../../../Common/colours";
 import ChatIcon from '@mui/icons-material/Chat';
+import { useNavigate } from "react-router-dom";
 
 const LetsConnectCard = () => {
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate("/contact"); // navigates to your contact page
+    };
+
     return (
         <CardContainer>
-            <Heading>Are You Ready to Kickstart Your Project with a Touch of Magic?</Heading>
+            <Heading>
+                Are You Ready to Kickstart Your Project with a Touch of Magic?
+            </Heading>
             <Description>
                 Reach out and let’s bring your ideas to life ✨.
                 I’m open to full-time and part-time opportunities to innovate, design, and deliver exceptional solutions.
             </Description>
             <ConnectButton
                 variant="contained"
-                href="contact"
+                onClick={handleNavigate}
                 endIcon={<ChatIcon />}
                 sx={{
                     backgroundColor: colour_white,
