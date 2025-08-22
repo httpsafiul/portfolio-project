df0676e367f0ad517258d88f3b468d1ece8190b6
Safiul Haque <mdsafiulhaque4@gmail.com>
2025-08-22 13:41:27 +0530

    update

--- src/components/LandingPage/LandingPageMain.jsx
+++ src/components/LandingPage/LandingPageMain.jsx
@@ -1,13 +1,8 @@
 import React, { Suspense } from 'react';
-// import HeroMain from './Hero/HeroMain';
 const HeroMain = React.lazy(() => import('./Hero/HeroMain'))
 const IntroductionMain = React.lazy(() => import('./Introduction/IntroductionMain'))
-// import IntroductionMain from './Introduction/IntroductionMain';
-// import EducationMain from './Education/EducationMain';
 const EducationMain = React.lazy(() => import('./Education/EducationMain'))
-// import TechStackMain from './TechStack/TechStackMain';
 const TechStackMain = React.lazy(() => import('./TechStack/TechStackMain'))
-// import LetsConnectMain from './LetsConnect/LetsConnectMain';
 const LetsConnectMain = React.lazy(() => import('./LetsConnect/LetsConnectMain'))
 import Navbar from '../Navbar/Navbar';
 import LoadingScreen from './LazyLoadingComponents/LoadingScreen';
@@ -27,8 +22,6 @@ function LandingPageMain() {
             height: "100%",
             background: "#f9f9f9",
             display: "flex",
-            // justifyContent: "center",
-            // alignItems: "center",
             flexDirection: "column",
             zIndex: 9999,
           }}
