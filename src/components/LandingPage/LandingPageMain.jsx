ae6f5f835befc177de30e4d67c839b0d65bc38a7
Safiul Haque <mdsafiulhaque4@gmail.com>
2025-08-22 16:32:42 +0530

    finallyyyy

--- src/components/LandingPage/LandingPageMain.jsx
+++ src/components/LandingPage/LandingPageMain.jsx
@@ -1,63 +1,20 @@
-import React, { Suspense } from 'react';
-const HeroMain = React.lazy(() => import('./Hero/HeroMain'))
-const IntroductionMain = React.lazy(() => import('./Introduction/IntroductionMain'))
-const EducationMain = React.lazy(() => import('./Education/EducationMain'))
-const TechStackMain = React.lazy(() => import('./TechStack/TechStackMain'))
-const LetsConnectMain = React.lazy(() => import('./LetsConnect/LetsConnectMain'))
+import React from 'react';
+import HeroMain from './Hero/HeroMain';
+import IntroductionMain from './Introduction/IntroductionMain';
+import EducationMain from './Education/EducationMain';
+import TechStackMain from './TechStack/TechStackMain';
+import LetsConnectMain from './LetsConnect/LetsConnectMain';
 import Navbar from '../Navbar/Navbar';
-import LoadingScreen from './LazyLoadingComponents/LoadingScreen';
-import { CircularProgress } from '@mui/material';
 
 function LandingPageMain() {
   return (
     <>
-      <Navbar active={"home"} />
-      <Suspense fallback={
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background: "#f9f9f9",
-            display: "flex",
-            flexDirection: "column",
-            zIndex: 9999,
-          }}
-        >
-          <LoadingScreen />
-        </div>
-      }>
+    <Navbar active={"home"}/>
         <HeroMain />
-      </Suspense>
-
-      <Suspense fallback={
-        <CircularProgress />
-      }>
         <IntroductionMain />
-      </Suspense>
-
-      <Suspense fallback={
-        <CircularProgress />
-      }>
-        <EducationMain />
-      </Suspense>
-
-
-      <Suspense fallback={
-        <CircularProgress />
-      }>
-        <TechStackMain />
-      </Suspense>
-
-
-      <Suspense fallback={
-        <CircularProgress />
-      }>
-        <LetsConnectMain />
-      </Suspense>
-
+        <EducationMain/>
+        <TechStackMain/>
+        <LetsConnectMain/>
     </>
   );
 }
